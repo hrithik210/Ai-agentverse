f00f85f70a8c0f050bf10b5fcc14d44b8a098e2d
hrithik210 <hrithiknayak7829@gmail.com>
2025-06-29 23:29:43 +0530

    added xp/level model

--- HigherMe/app/db/models.py
+++ HigherMe/app/db/models.py
@@ -27,4 +27,20 @@ class MoodLog(Base):
     id = Column(Integer, primary_key=True, index=True)
     mood_text = Column(String)
     sentiment = Column(String)
-    date = Column(DateTime, default=datetime.utcnow)
+    date = Column(DateTime, default=datetime.now)
+    
+class XPEvent(Base):
+    __tablename__ = "xp_events"
+    id = Column(Integer, primary_key=True, index=True)
+    xp_type = Column(String)
+    amount = Column(Integer)
+    timestamp = Column(DateTime, default=datetime.now)
+    
+
+class Level(Base):
+    __tablename__ = "levels"
+    id = Column(Integer, primary_key=True, index=True)
+    current_level = Column(Integer, default=1)
+    total_xp = Column(Integer, default=0)
+    last_updated = Column(DateTime, default=datetime.now)
+    
