23e5480e5f1c1e799b327a463d1f6851e93d6acf
hrithik210 <hrithiknayak7829@gmail.com>
2025-06-29 23:46:18 +0530

    added query to add create health log

--- HigherMe/app/db/crud.py
+++ HigherMe/app/db/crud.py
@@ -14,3 +14,17 @@ def create_code_log(db: Session , lines_Added : int , lines_Removed : int , tota
   db.commit()
   db.refresh(code_log)
   return code_log
+
+def create_health_log(db: Session, meals: str, sleep_hours: float, exercise_minutes: float, water_intake_liter: float):
+  health_log = models.HealthLog(
+    meals = meals,
+    sleep_hours = sleep_hours,
+    excercise_minutes = exercise_minutes,
+    water_intake_liter = water_intake_liter,
+    date = datetime.now()
+  )
+  
+  db.add(health_log)
+  db.commit()
+  db.refresh(health_log)
+  return health_log
