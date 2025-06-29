f8fb4854636feedbaa68ac44d8b481f5c9c7a4e0
hrithik210 <hrithiknayak7829@gmail.com>
2025-06-29 16:24:30 +0530

    added db stuff

--- HigherMe/app/db/database.py
+++ HigherMe/app/db/database.py
@@ -7,3 +7,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DB_URL", "")
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+sessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+  db = sessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
+
+
+def test_db_connection():
+  try:
+    with engine.connect() as connection:
+      print("Database connection successful!")
+      return True
+  except Exception as e:
+    print(f"Database connection failed: {e}")
+    return False
+
+
+
+test_db_connection()
