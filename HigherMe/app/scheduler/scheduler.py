70e6eab0747ba3c77544b93cb5b0740f589fb31a
hrithik210 <hrithiknayak7829@gmail.com>
2025-07-13 01:05:26 +0530

    scheduler fix

--- HigherMe/app/scheduler/scheduler.py
+++ HigherMe/app/scheduler/scheduler.py
@@ -1,5 +1,10 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.agents.mood_agent import  calculate_daily_mood_xp
+from app.agents.code_agent import run_code_agent
+from app.agents.health_agent import run_health_agent
+
+from app.agents.daily_report_agent import build_daily_report
+
 from app.db.database import get_db
 import logging
 
@@ -10,9 +15,14 @@ scheduler = BackgroundScheduler()
 
 
 def run_all_daily_tasks():
-    with next(get_db()) as db:
+    with get_db() as db:
+        print("🧠 Running daily tasks...")
         print("🧠 Running daily XP + report tasks...")
         calculate_daily_mood_xp()
+        run_code_agent()
+        run_health_agent()
+        build_daily_report(db)
+        
         
 
 def start():
