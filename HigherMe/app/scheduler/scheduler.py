da1529910e8eeec38b66b5e1897ad2b3754ed6d1
hrithik210 <hrithiknayak7829@gmail.com>
2025-07-13 00:03:01 +0530

    added scheduler

--- HigherMe/app/scheduler/scheduler.py
+++ HigherMe/app/scheduler/scheduler.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from app.agents import mood_xp_llm_runner, health_xp_runner, code_xp_runner, daily_report_agent
+from app.agents.mood_agent import  calculate_daily_mood_xp
 from app.db.database import get_db
 import logging
 
@@ -7,3 +7,14 @@ logging.basicConfig()
 logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 scheduler = BackgroundScheduler()
+
+
+def run_all_daily_tasks():
+    with next(get_db()) as db:
+        print("🧠 Running daily XP + report tasks...")
+        calculate_daily_mood_xp()
+        
+
+def start():
+    scheduler.add_job(run_all_daily_tasks, 'cron', hour=23, minute=59)
+    scheduler.start()
