aef78cf6a31b853cac4ac1d5e79a2bb7c5aa5283
hrithik210 <hrithiknayak7829@gmail.com>
2025-07-17 23:52:14 +0530

    Added route to log moods

--- HigherMe/app/api/main.py
+++ HigherMe/app/api/main.py
@@ -1,11 +1,33 @@
 from fastapi import FastAPI
 from app.scheduler.scheduler import start
+from requests import Request
+from app.db.database import get_db_session
+from app.agents.mood_agent import log_mood
 
 
 app = FastAPI(
     title="HigherMe API",
     description="API for HigherMe application",
 )
+
+
+@app.post("/log-mood")
+async def log_mood(request  : Request):
+    
+    db = get_db_session()
+    
+    try:
+        data = await request.json()
+        mood_text = data.get("mood_text" , "")
+        
+        print(f"mood text from req: {mood_text}")
+        
+        log_mood(mood_text)
+        print("Mood logged successfully")
+        return {"message": "Mood logged successfully"}
+    except Exception as e:
+        print(f"Error logging mood: {e}")
+        return {"error": "Failed to log mood"}
 
 
 @app.on_event("startup")
