6b26593b90a7e23acddba6db48a9c1c1b8faeb99
hrithik210 <hrithiknayak7829@gmail.com>
2025-07-17 23:56:36 +0530

    added route to log meals

--- HigherMe/app/api/main.py
+++ HigherMe/app/api/main.py
@@ -3,6 +3,7 @@ from app.scheduler.scheduler import start
 from requests import Request
 from app.db.database import get_db_session
 from app.agents.mood_agent import log_mood
+import json
 
 
 app = FastAPI(
@@ -29,6 +30,21 @@ async def log_mood(request  : Request):
         print(f"Error logging mood: {e}")
         return {"error": "Failed to log mood"}
 
+
+@app.post("/log-meals")
+async def log_meals(request: Request):
+    db = get_db_session()
+    
+    try:
+        data = await request.json()
+        meal = data.get("meal" , "")
+        print(f"meal from request  : {meal}")
+        log_meals(meal)
+        print("Meals logged successfully")
+        return json.response({"message": "Meals logged successfully"})
+    except Exception as e:
+        print(f"Error logging meals: {e}")
+        return json.response({"error" : "Failed to log meals"})
 
 @app.on_event("startup")
 async def startup_event():
